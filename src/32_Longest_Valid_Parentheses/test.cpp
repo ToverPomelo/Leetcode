b996731a97e9ebf96486b04d02f12b8338dfdee1
Tover <tover@tover.xyz>
2019-03-06 17:42:13 +0800

    mid backup

--- src/32_Longest_Valid_Parentheses/test.cpp
+++ src/32_Longest_Valid_Parentheses/test.cpp
@@ -8,41 +8,8 @@ public:
         return a>b ? a : b;
     }
 
-	bool isNum(int i){
-		return i>=0;
-	}
-
 	bool bothNumber(int last,int now){
-		return last>=0 && now>=0;
-	}
-
-	int c2iFilter(char c){
-		if(c == '(') return -1;
-		else if(c == ')') return -2;
-		else return (int)c;
-	}
-
-	char i2cFilter(int i){
-		if(i == -1) return '(';
-		else if(i == -2) return ')';
-		else return (char)i;
-	}
-
-	void mergeTopNum(stack<int>& st){
-		while(st.size() >= 2){  //sum numbers
-			int lastTop = st.top();
-			st.pop();
-			int nowTop = st.top();
-			st.pop();
-			
-			if(bothNumber(lastTop,nowTop)){
-				st.push(lastTop+nowTop);
-			}else{
-				st.push(nowTop);
-				st.push(lastTop);
-				break;
-			}
-		}
+		return (char)last!='(' && (char)last!=')' && (char)now!='(' && (char)now!=')';
 	}
 
     int longestValidParentheses(string s) {
@@ -50,38 +17,60 @@ public:
 
         for(char si : s){
             if(si == '('){
-                st.push(c2iFilter(si));
+                st.push(int(si));
             }else if(si == ')'){
-				mergeTopNum(st);
+				int lastTop = 0;
+				if(!st.empty()){
+					lastTop = st.top();
+					st.pop();
+				}else{
+					st.push((int)')');
+					continue;
+				}
 
 				int tmpNum = 0;
-				if(!st.empty() && bothNumber(st.top(),0)){
-					tmpNum = st.top();
-					st.pop();
+				if(!bothNumber(lastTop,0)){ //not a number
+					st.push(lastTop);
+				}else{
+					while(!st.empty() && bothNumber(lastTop,st.top())){  //sum numbers(final lastTop)
+						lastTop = st.top()+lastTop;
+						st.pop();
+					}
+					tmpNum = lastTop;
 				}
-				//if(!st.empty()) cout<<(char)st.top();
 
-				if(!st.empty()) cout<<i2cFilter(st.top());
-				if(!st.empty() && i2cFilter(st.top())=='('){
+				if(!st.empty() && (char)st.top()=='('){
 					st.pop();
 					//cout<<"test1: "<<tmpNum<<endl;
 					st.push(tmpNum+2);
-					//st.push(2);
 				}else{
 					if(tmpNum) st.push(tmpNum);
-					st.push(c2iFilter(si));
+					st.push((int)')');
 				}
             }
         }
-		cout<<endl;
 
 		int result = 0;
 		while(!st.empty()){
-			mergeTopNum(st);
+			while(st.size() >= 2){  //sum numbers
+				int lastTop = st.top();
+				st.pop();
+				int nowTop = st.top();
+				st.pop();
+				
+				if(bothNumber(lastTop,nowTop)){
+					st.push(lastTop+nowTop);
+				}else{
+					st.push(nowTop);
+					st.push(lastTop);
+					break;
+				}
+			}
 
 			int top = st.top();
-			if(!isNum(top))
-				cout<<"test2: "<<i2cFilter(top)<<endl;
+
+			if((char)top=='(' || (char)top==')')
+				cout<<"test2: "<<(char)top<<endl;
 			else{
 				result = max(result,top);
 				cout<<"test2: "<<top<<endl;
@@ -100,8 +89,6 @@ int main(){
     //string test = "))(())))";
     //string test = "))(((())(()))()))(()((((((())))))))((((";
     string test = ")(()(()(((())(((((()()))((((()()(()()())())())()))()()()())(())()()(((()))))()((()))(((())()((()()())((())))(())))())((()())()()((()((())))))((()(((((()((()))(()()(())))((()))()))())";
-	
-	cout<<test.length()<<endl;
 
     Solution* s = new Solution();
     cout<<(s->longestValidParentheses(test))<<endl;
